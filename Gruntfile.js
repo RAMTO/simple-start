d748216556fc6974563f4e5ba7d8699505805211
Martin Dobrev <martindobrev87@gmail.com>
2017-09-13 17:59:06 +0300

    First Commit

---
+++ Gruntfile.js
@@ -0,0 +1,89 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        browserSync: {
+            bsFiles: {
+                src : [
+                    'dist/css/*.css',
+                    'dist/*.html',
+                    'dist/*.js'
+                ]
+            },
+            options: {
+                watchTask: true,
+                port: 3000,
+                server: {
+                    baseDir: "dist"
+                }
+            }
+        },
+
+        copy: {
+            images: {
+                expand: true,
+                cwd: 'src/images/',
+                src: '**/*.{png,jpg,gif,ico,svg}',
+                dest: 'dist/images/'
+            },
+            scripts: {
+                expand: true,
+                cwd: 'src/js/',
+                src: '**/*.{js,jsx}',
+                dest: 'dist/js/'
+            },
+            fonts: {
+                expand: true,
+                cwd: 'src/fonts/',
+                src: '**/*.{eot,woff,woff2,ttf,svg}',
+                dest: 'dist/fonts/'
+            }
+        },
+
+        codekit: {    
+            target: {
+                src : 'src/templates/*.kit',
+                dest : 'dist/'
+            }
+        },
+
+        sass: {
+            options:{
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'dist/css/style.css': 'src/scss/style.scss'
+                }
+            }
+        },
+
+        autoprefixer:{
+            dist:{
+                files:{
+                    'dist/css/style.css':'dist/css/style.css'
+                }
+            }
+        },
+
+        watch: {
+            css: {
+                files: '**/*.scss',
+                tasks: ['sass', 'autoprefixer']
+            },
+            js: {
+                files: '**/*.js',
+                tasks: ['copy:scripts']
+            },
+            kit: {
+                files: '**/*.kit',
+                tasks: ['codekit']
+            }
+        }
+
+    });
+    
+    require('load-grunt-tasks')(grunt);
+    grunt.registerTask('build', ['codekit', 'copy', 'sass', 'autoprefixer']);
+    grunt.registerTask('default', ['browserSync', 'watch']);
+}
